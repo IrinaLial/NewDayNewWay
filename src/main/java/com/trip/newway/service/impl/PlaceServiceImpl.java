bf6a3f60650fbd0257a3e0036b793faba1d6b431
Iryna Lialenko <lyalenko.iryna@gmail.com>
2019-02-19 19:43:25 +0200

    init changes

--- src/main/java/com/trip/newway/service/impl/PlaceServiceImpl.java
+++ src/main/java/com/trip/newway/service/impl/PlaceServiceImpl.java
@@ -4,7 +4,7 @@ import com.trip.newway.dto.place.PlaceDTO;
 import com.trip.newway.dto.place.ResponsePlaceDTO;
 import com.trip.newway.dto.place.SavedPlaceDTO;
 import com.trip.newway.model.Place;
-import com.trip.newway.repository.PlaceReposirory;
+import com.trip.newway.repository.PlaceRepository;
 import com.trip.newway.service.PlaceService;
 import com.trip.newway.util.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +18,7 @@ import java.util.List;
 @Service
 public class PlaceServiceImpl implements PlaceService {
     @Autowired
-    private PlaceReposirory placeReposirory;
+    private PlaceRepository placeRepository;
 
     @Override
     public PlaceDTO save(SavedPlaceDTO placeDTO) {
@@ -28,26 +28,26 @@ public class PlaceServiceImpl implements PlaceService {
         place.setLatitude(placeDTO.getLatitude());
         place.setLongitude(placeDTO.getLongitude());
 
-        Place savedPlace = placeReposirory.save(place);
+        Place savedPlace = placeRepository.save(place);
         return new PlaceDTO(savedPlace.getId(),savedPlace.getName(),savedPlace.getLatitude(),savedPlace.getLongitude());
     }
     @Override
     public ResponsePlaceDTO findAll(int page) {
-        List<Place> places = placeReposirory
+        List<Place> places = placeRepository
                 .findAll(PageRequest.of(page, Constants.LIMIT)).getContent();
         List<PlaceDTO> placeDTOS = new LinkedList<>();
         places.forEach(s->{
             PlaceDTO placeDTO = new PlaceDTO(s.getId(),s.getName(),s.getLatitude(),s.getLongitude());
             placeDTOS.add(placeDTO);
         });
-        long count = placeReposirory.count();
+        long count = placeRepository.count();
 
         return new ResponsePlaceDTO(placeDTOS,count);
     }
 
     @Override
     public PlaceDTO findByName(String name) {
-        return placeReposirory.findByName(name);
+        return placeRepository.findByName(name);
     }
 }
 //todo ask about "findByName"
