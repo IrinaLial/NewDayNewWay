6938f111a822bd1ffe355d9a591f01fb54216590
Iryna Lialenko <lyalenko.iryna@gmail.com>
2019-06-12 15:29:07 +0300

    authorization

--- src/main/java/com/trip/newway/dto/car/CarDTO.java
+++ src/main/java/com/trip/newway/dto/car/CarDTO.java
@@ -1,11 +1,7 @@
 package com.trip.newway.dto.car;
 
-import com.trip.newway.model.Car;
 import lombok.*;
 
-/**
- * The {@link CarDTO} to read a {@link Car } entity by controller.
- */
 
 @Getter
 @Setter
